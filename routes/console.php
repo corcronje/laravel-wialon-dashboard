75e76496767af4ebedd8b6aa1d437391d5896799
Cor Cronje <cor@nuvoteq.io>
2024-06-02 19:53:31 +0200

    [main] Schedule

--- routes/console.php
+++ routes/console.php
@@ -6,3 +6,8 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
+
+
+Artisan::command('import:units', function () {
+    $this->comment('Units imported successfully');
+})->purpose('Sync Wialon unit messages')->everyFiveMinutes();
