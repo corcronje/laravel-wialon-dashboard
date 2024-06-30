f80e7eaa21e638ef6852931a49f12127f0851b8e
Cor Cronje <cronje.cor@gmail.com>
2024-06-30 20:06:29 +0200

    [main] Update

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -3,14 +3,16 @@
 @section('page-title')
     <div class="d-flex justify-content-between align-items-center pb-3 mb-3 border-bottom">
         <h2 class="m-0">Dashboard</h2>
-        @can('create', \App\Models\Order::class)
-            <a href="{{ route('orders.create') }}" class="btn btn-primary">New Order</a>
-        @endcan
     </div>
 @endsection
 
 @section('content')
-    <h4>Recent Orders</h4>
+    <div class="d-flex justify-content-between align-items-center">
+        <h4>Recent Orders</h4>
+        @can('create', \App\Models\Order::class)
+            <a href="{{ route('orders.create') }}" class="btn btn-sm btn-primary">New Order</a>
+        @endcan
+    </div>
     @include('orders._datatable', [
         'config' => [
             'order' => [0, 'desc'],
@@ -20,7 +22,12 @@
             'ordering' => false,
         ],
     ])
-    <h4 class="mt-5">Recent Trips</h4>
+    <div class="d-flex justify-content-between align-items-center">
+        <h4>Recent Trips</h4>
+        @can('create', \App\Models\Trip::class)
+            <a href="{{ route('trips.create') }}" class="btn btn-sm btn-primary">New Trip</a>
+        @endcan
+    </div>
     @include('trips._datatable', [
         'config' => [
             'order' => [0, 'desc'],
