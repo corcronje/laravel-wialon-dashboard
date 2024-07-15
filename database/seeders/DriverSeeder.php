2d07bbd7f8c28df6fd0f014f8437c945322b7c81
Cor Cronje <cor@phpweb.co.za>
2024-07-15 19:45:16 +0200

    [main] Update importer

---
+++ database/seeders/DriverSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Driver;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+
+class DriverSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // open the csv file for reading
+        $file = fopen(storage_path('app/drivers.csv'), 'r');
+
+        // read the first row of the csv file
+        $headers = fgetcsv($file);
+
+        // loop through the rest of the csv file
+        while ($row = fgetcsv($file)) {
+            // combine the headers and the row into an associative array
+            $data = array_combine($headers, $row);
+
+            // create a new driver record
+            Driver::updateOrCreate([
+                'employee_number' => $data['employee_number'],
+            ],[
+                'name' => Str::title($data['firstname'] . ' ' . Str::title($data['lastname'])),
+            ]);
+        }
+    }
+}
