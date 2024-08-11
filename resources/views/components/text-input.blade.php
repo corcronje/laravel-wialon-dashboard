2c4134cafa84808afe9aa9188a3b25a5ca7ceb32
Cor Cronje <cor@phpweb.co.za>
2024-08-11 14:48:29 +0200

    [main] Add html closure slash

--- resources/views/components/text-input.blade.php
+++ resources/views/components/text-input.blade.php
@@ -1,7 +1,7 @@
 @props(['name', 'title', 'value' => '', 'placeholder' => ''])
 <div class="mb-3">
     <label for="{{ $name }}" class="form-label">{{ $title }}</label>
-    <input name="{{ $name }}" type="text" class="form-control" id="{{ $name }}" value="{{ $value }}" placeholder="{{ $placeholder }}">
+    <input name="{{ $name }}" type="text" class="form-control" id="{{ $name }}" value="{{ $value }}" placeholder="{{ $placeholder }}" />
     @error($name)
         <div class="text-danger">{{ $message }}</div>
     @enderror
