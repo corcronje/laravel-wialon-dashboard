2ed94018703e0bde109bccb08d769b3db7075c26
Cor Cronje <cor@phpweb.co.za>
2024-08-11 14:08:05 +0200

    [main] Add requests

---
+++ app/Http/Requests/StorePumpRequest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Models\Pump;
+use Illuminate\Foundation\Http\FormRequest;
+
+class StorePumpRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return $this->user()->can('create', Pump::class);
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'guid' => ['required', 'string', 'unique:pumps'],
+            'name' => ['required', 'string'],
+            'description' => ['sometimes', 'string'],
+            'cents_per_litre' => ['required', 'integer'],
+            'volume_litres' => ['required', 'integer'],
+            'pulses_per_litre' => ['required', 'integer'],
+            'status' => ['required', 'string'],
+        ];
+    }
+}
---
+++ app/Http/Requests/UpdatePumpRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdatePumpRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return $this->user()->can('update', $this->route('pump'));
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'guid' => ['required', 'string', 'unique:pumps'],
+            'name' => ['required', 'string'],
+            'description' => ['sometimes', 'string'],
+            'cents_per_litre' => ['required', 'integer'],
+            'volume_litres' => ['required', 'integer'],
+            'pulses_per_litre' => ['required', 'integer'],
+            'status' => ['required', 'string'],
+        ];
+    }
+}
