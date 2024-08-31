44fa2697d82b1956b788f9cce363faa2b294556d
Cor Cronje <cor@phpweb.co.za>
2024-08-31 16:57:03 +0200

    [issue-5] Update transaction policy

--- app/Policies/TransactionPolicy.php
+++ app/Policies/TransactionPolicy.php
@@ -11,56 +11,76 @@ class TransactionPolicy
     /**
      * Determine whether the user can view any models.
      */
-    public function viewAny(User $user): bool
+    public function viewAny(User $user): Response
     {
-        //
+        return Response::allow();
     }
 
     /**
      * Determine whether the user can view the model.
      */
-    public function view(User $user, Transaction $transaction): bool
+    public function view(User $user, Transaction $transaction): Response
     {
-        //
+        return Response::allow();
     }
 
     /**
      * Determine whether the user can create models.
      */
-    public function create(User $user): bool
+    public function create(User $user): Response
     {
-        //
+        if ($user->isAdmin()) {
+            return Response::allow();
+        }
+
+        return Response::deny('You do not have permission to create a transaction');
     }
 
     /**
      * Determine whether the user can update the model.
      */
-    public function update(User $user, Transaction $transaction): bool
+    public function update(User $user, Transaction $transaction): Response
     {
-        //
+        if ($user->isAdmin()) {
+            return Response::allow();
+        }
+
+        return Response::deny('You do not have permission to update a transaction');
     }
 
     /**
      * Determine whether the user can delete the model.
      */
-    public function delete(User $user, Transaction $transaction): bool
+    public function delete(User $user, Transaction $transaction): Response
     {
-        //
+        if ($user->isAdmin()) {
+            return Response::allow();
+        }
+
+        return Response::deny('You do not have permission to delete a transaction');
     }
 
     /**
      * Determine whether the user can restore the model.
      */
-    public function restore(User $user, Transaction $transaction): bool
+    public function restore(User $user, Transaction $transaction): Response
     {
-        //
+        if ($user->isAdmin()) {
+            return Response::allow();
+        }
+
+        return Response::deny('You do not have permission to restore a transaction');
     }
 
     /**
      * Determine whether the user can permanently delete the model.
      */
-    public function forceDelete(User $user, Transaction $transaction): bool
+    public function forceDelete(User $user, Transaction $transaction): Response
     {
-        //
+        if ($user->isAdmin()) {
+            return Response::allow();
+        }
+
+        return Response::deny('You do not have permission to permanently delete a transaction');
     }
 }
