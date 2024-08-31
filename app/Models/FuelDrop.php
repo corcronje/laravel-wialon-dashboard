4820f38c3e7b26613c97ebc61fda446cb9f6d117
Cor Cronje <cor@phpweb.co.za>
2024-08-31 16:06:42 +0200

    [issue-11] Add fuel drop model

---
+++ app/Models/FuelDrop.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class FuelDrop extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'user_id',
+        'tank_id',
+        'volume_in_litres',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function tank()
+    {
+        return $this->belongsTo(Tank::class);
+    }
+}
