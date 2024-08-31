1291c67d8621eb4291cfc4ff882b265a3f17f3a8
Cor Cronje <cor@phpweb.co.za>
2024-08-31 17:17:14 +0200

    [issue-5] Update transaction factory

--- database/factories/TransactionFactory.php
+++ database/factories/TransactionFactory.php
@@ -2,6 +2,10 @@
 
 namespace Database\Factories;
 
+use App\Models\Driver;
+use App\Models\Pump;
+use App\Models\Unit;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -16,8 +20,34 @@ class TransactionFactory extends Factory
      */
     public function definition(): array
     {
+        $user = User::all()->random();
+        $driver = Driver::all()->random();
+        $pump = Pump::all()->random();
+        $unit = Unit::all()->random();
+
+        if (!$user) {
+            $user = User::factory()->create();
+        }
+
+        if (!$driver) {
+            $driver = Driver::factory()->create();
+        }
+
+        if (!$pump) {
+            $pump = Pump::factory()->create();
+        }
+
+        if (!$unit) {
+            $unit = Unit::factory()->create();
+        }
+
         return [
-            //
+            'user_id' => $user->id,
+            'driver_id' => $driver->id,
+            'unit_id' => $unit->id,
+            'pump_id' => $pump->id,
+            'volume_in_liters' => random_int(10 * 1000, 100 * 1000),
+            'amount_in_cents' => random_int(1 * 1000, 10 * 1000),
         ];
     }
 }
