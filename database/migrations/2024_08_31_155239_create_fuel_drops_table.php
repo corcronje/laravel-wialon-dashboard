609bf84c864a28e7692f354e1852cb9371a09550
Cor Cronje <cor@phpweb.co.za>
2024-08-31 16:06:59 +0200

    [issue-11] Add create fuel drops table migration

---
+++ database/migrations/2024_08_31_155239_create_fuel_drops_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use App\Models\Tank;
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('fuel_drops', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(User::class)->constrained();
+            $table->foreignIdFor(Tank::class)->constrained();
+            $table->integer('volume_in_litres');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('fuel_drops');
+    }
+};
