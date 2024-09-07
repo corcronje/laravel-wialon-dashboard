ab8a9470827865975c38acd9f9d049b28010f0e5
Cor Cronje <cor@phpweb.co.za>
2024-09-07 13:22:39 +0200

    [task-15] Pump belongs to tank

--- app/Models/Pump.php
+++ app/Models/Pump.php
@@ -2,17 +2,19 @@
 
 namespace App\Models;
 
+use App\Traits\BelongsToTank;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Pump extends Model
 {
-    use HasFactory, SoftDeletes;
+    use HasFactory, SoftDeletes, BelongsToTank;
 
     protected $fillable = [
         'guid',
         'name',
+        'tank_id',
         'description',
         'cents_per_litre',
         'volume_litres',
