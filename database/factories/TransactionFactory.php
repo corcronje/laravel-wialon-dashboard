a945800e809ab18d555ce778269930b72b69da0f
Cor Cronje <cor@phpweb.co.za>
2024-09-07 15:35:03 +0200

    [task-18] Transactions belongs to transaction type

--- database/factories/TransactionFactory.php
+++ database/factories/TransactionFactory.php
@@ -4,6 +4,7 @@ namespace Database\Factories;
 
 use App\Models\Driver;
 use App\Models\Pump;
+use App\Models\TransactionType;
 use App\Models\Unit;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
@@ -24,6 +25,7 @@ class TransactionFactory extends Factory
         $driver = Driver::all()->random();
         $pump = Pump::all()->random();
         $unit = Unit::all()->random();
+        $type = TransactionType::all()->random();
 
         if (!$user) {
             $user = User::factory()->create();
@@ -41,12 +43,18 @@ class TransactionFactory extends Factory
             $unit = Unit::factory()->create();
         }
 
+        if (!$type) {
+            $type = TransactionType::factory()->create();
+        }
+
         return [
             'user_id' => $user->id,
             'driver_id' => $driver->id,
             'unit_id' => $unit->id,
             'pump_id' => $pump->id,
-            'volume_in_liters' => random_int(10 * 1000, 100 * 1000),
+            'transaction_type_id' => $type->id,
+            'description' => $this->faker->sentence,
+            'volume_in_millilitres' => random_int(100 * 1000, 1000 * 1000),
             'amount_in_cents' => random_int(1 * 1000, 10 * 1000),
         ];
     }
