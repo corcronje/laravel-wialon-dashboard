a25f9694bb2e265c6413e6d50b39a60b82cf61fa
Cor Cronje <cor@phpweb.co.za>
2024-09-07 13:41:23 +0200

    [task-15] Fuel adjustments are imutable

--- app/Http/Controllers/FuelAdjustmentController.php
+++ app/Http/Controllers/FuelAdjustmentController.php
@@ -2,11 +2,9 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\StoreFuelAdjustmentRequest;
-use App\Http\Requests\UpdateFuelAdjustmentRequest;
+use App\Http\Requests\FuelAdjustment\StoreFuelAdjustmentRequest;
 use App\Models\FuelAdjustment;
 use App\Models\Tank;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Gate;
 
 class FuelAdjustmentController extends Controller
@@ -53,39 +51,5 @@ class FuelAdjustmentController extends Controller
         Gate::authorize('view', $adjustment);
 
         return view('adjustments.show', compact('adjustment'));
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(FuelAdjustment $adjustment)
-    {
-        Gate::authorize('update', $adjustment);
-
-        $tanks = Tank::all()->pluck('name', 'id');
-
-        return view('adjustments.edit', compact('adjustment', 'tanks'));
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(UpdateFuelAdjustmentRequest $request, FuelAdjustment $adjustment)
-    {
-        $adjustment->update($request->validated());
-
-        return redirect()->route('adjustments.index')->with('success', 'Fuel adjustment updated successfully');
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(FuelAdjustment $adjustment)
-    {
-        Gate::authorize('delete', $adjustment);
-
-        $adjustment->delete();
-
-        return redirect()->route('adjustments.index')->with('success', 'Fuel adjustment deleted successfully');
     }
 }
