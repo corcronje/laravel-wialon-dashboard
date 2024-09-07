813b354c32e4203282762ea5921228a38659dd61
Cor Cronje <cor@phpweb.co.za>
2024-09-07 15:32:46 +0200

    [task-15] Remove redundant tests due to models being imutable

--- tests/Feature/FuelDipTest.php
+++ tests/Feature/FuelDipTest.php
@@ -52,54 +52,6 @@ class FuelDipTest extends TestCase
         $response->assertStatus(200);
     }
 
-    // a user cannot view the fuel dips edit page
-    public function test_user_cannot_view_the_fuel_dips_edit_page()
-    {
-        $user = $this->newUser();
-
-        $dip = FuelDip::factory()->create();
-
-        $response = $this->actingAs($user)->get(route('dips.edit', $dip));
-
-        $response->assertStatus(403);
-    }
-
-    // a admin user can view the fuel dips edit page
-    public function test_admin_user_can_view_the_fuel_dips_edit_page()
-    {
-        $user = $this->newAdminUser();
-
-        $dip = FuelDip::factory()->create();
-
-        $response = $this->actingAs($user)->get(route('dips.edit', $dip));
-
-        $response->assertStatus(200);
-    }
-
-    // a user cannot delete a fuel dip
-    public function test_user_cannot_delete_a_fuel_dip()
-    {
-        $user = $this->newUser();
-
-        $dip = FuelDip::factory()->create();
-
-        $response = $this->actingAs($user)->delete(route('dips.destroy', $dip));
-
-        $response->assertStatus(403);
-    }
-
-    // a admin user can delete a fuel dip
-    public function test_admin_user_can_delete_a_fuel_dip()
-    {
-        $user = $this->newAdminUser();
-
-        $dip = FuelDip::factory()->create();
-
-        $response = $this->actingAs($user)->delete(route('dips.destroy', $dip));
-
-        $response->assertStatus(302);
-    }
-
     // a user cannot create a fuel dip
     public function test_user_cannot_create_a_fuel_dip()
     {
@@ -126,38 +78,6 @@ class FuelDipTest extends TestCase
             'tank_id' => $tank->id,
             'volume_in_litres' => 1000
         ]);
-
-        $response->assertStatus(302);
-    }
-
-    // a user cannot update a fuel dip
-    public function test_user_cannot_update_a_fuel_dip()
-    {
-        $user = $this->newUser();
-
-        $dip = FuelDip::factory()->create();
-
-        $data = FuelDip::factory()->make();
-
-        $response = $this->actingAs($user)->put(route('dips.update', $dip), $data->toArray());
-
-        $response->assertStatus(403);
-    }
-
-    // a admin user can update a fuel dip
-    public function test_admin_user_can_update_a_fuel_dip()
-    {
-        $user = $this->newAdminUser();
-
-        $dip = FuelDip::factory()->create();
-
-        $data = FuelDip::factory()->make();
-
-        $response = $this->actingAs($user)->put(route('dips.update', $dip), $data->toArray());
-
-        $updated = FuelDip::find($dip->id);
-
-        $this->assertEquals($data->volume_in_litres, $updated->volume_in_litres);
 
         $response->assertStatus(302);
     }
--- tests/Feature/FuelDropTest.php
+++ tests/Feature/FuelDropTest.php
@@ -52,54 +52,6 @@ class FuelDropTest extends TestCase
         $response->assertStatus(200);
     }
 
-    // a user cannot view the fuel drops edit page
-    public function test_user_cannot_view_the_fuel_drops_edit_page()
-    {
-        $user = $this->newUser();
-
-        $drop = FuelDrop::factory()->create();
-
-        $response = $this->actingAs($user)->get(route('drops.edit', $drop));
-
-        $response->assertStatus(403);
-    }
-
-    // a admin user can view the fuel drops edit page
-    public function test_admin_user_can_view_the_fuel_drops_edit_page()
-    {
-        $user = $this->newAdminUser();
-
-        $drop = FuelDrop::factory()->create();
-
-        $response = $this->actingAs($user)->get(route('drops.edit', $drop));
-
-        $response->assertStatus(200);
-    }
-
-    // a user cannot delete a fuel drop
-    public function test_user_cannot_delete_a_fuel_drop()
-    {
-        $user = $this->newUser();
-
-        $drop = FuelDrop::factory()->create();
-
-        $response = $this->actingAs($user)->delete(route('drops.destroy', $drop));
-
-        $response->assertStatus(403);
-    }
-
-    // a admin user can delete a fuel drop
-    public function test_admin_user_can_delete_a_fuel_drop()
-    {
-        $user = $this->newAdminUser();
-
-        $drop = FuelDrop::factory()->create();
-
-        $response = $this->actingAs($user)->delete(route('drops.destroy', $drop));
-
-        $response->assertStatus(302);
-    }
-
     // a user cannot create a fuel drop
     public function test_user_cannot_create_a_fuel_drop()
     {
@@ -126,38 +78,6 @@ class FuelDropTest extends TestCase
             'tank_id' => $tank->id,
             'volume_in_litres' => 1000
         ]);
-
-        $response->assertStatus(302);
-    }
-
-    // a user cannot update a fuel drop
-    public function test_user_cannot_update_a_fuel_drop()
-    {
-        $user = $this->newUser();
-
-        $drop = FuelDrop::factory()->create();
-
-        $data = FuelDrop::factory()->make();
-
-        $response = $this->actingAs($user)->put(route('drops.update', $drop), $data->toArray());
-
-        $response->assertStatus(403);
-    }
-
-    // a admin user can update a fuel drop
-    public function test_admin_user_can_update_a_fuel_drop()
-    {
-        $user = $this->newAdminUser();
-
-        $drop = FuelDrop::factory()->create();
-
-        $data = FuelDrop::factory()->make();
-
-        $response = $this->actingAs($user)->put(route('drops.update', $drop), $data->toArray());
-
-        $updated = FuelDrop::find($drop->id);
-
-        $this->assertEquals($data->volume_in_litres, $updated->volume_in_litres);
 
         $response->assertStatus(302);
     }
