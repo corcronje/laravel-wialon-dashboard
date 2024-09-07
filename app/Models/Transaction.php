e09e2e41ad9c49b742afd83dd3cde33ebeda07d2
Cor Cronje <cor@phpweb.co.za>
2024-09-07 15:36:11 +0200

    [task-15] Refactor to traits and update unit of measure to millilitres

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -2,40 +2,26 @@
 
 namespace App\Models;
 
+use App\Traits\BelongsToDriver;
+use App\Traits\BelongsToPump;
+use App\Traits\BelongsToUnit;
+use App\Traits\BelongsToUser;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Transaction extends Model
 {
-    use HasFactory, SoftDeletes;
+    use HasFactory, SoftDeletes, BelongsToUser, BelongsToDriver, BelongsToPump, BelongsToUnit;
 
     protected $fillable = [
         'user_id',
         'driver_id',
         'unit_id',
         'pump_id',
-        'volume_in_liters',
+        'transaction_type_id',
+        'description',
+        'volume_in_millilitres',
         'amount_in_cents',
     ];
-
-    public function user()
-    {
-        return $this->belongsTo(User::class);
-    }
-
-    public function driver()
-    {
-        return $this->belongsTo(Driver::class);
-    }
-
-    public function unit()
-    {
-        return $this->belongsTo(Unit::class);
-    }
-
-    public function pump()
-    {
-        return $this->belongsTo(Pump::class);
-    }
 }
