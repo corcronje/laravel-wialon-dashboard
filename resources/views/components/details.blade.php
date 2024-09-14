7cac4c5c446a425617dae4c6eca2af254d90a7d2
Cor Cronje <cor@phpweb.co.za>
2024-09-14 16:15:47 +0200

    [main] Refactor array to string conversion

--- resources/views/components/details.blade.php
+++ resources/views/components/details.blade.php
@@ -3,13 +3,12 @@
 @if (count($items) > 0)
     <dl class="row">
         @foreach ($items as $label => $value)
-            @php
-                if (is_array($value)) {
-                    $value = implode(', ', $value);
-                }
-            @endphp
-            <dt class="col-12 col-md-4 col-lg-3 text-md-end">{{ $label }}</dt>
-            <dd class="col-12 col-md-8 col-lg-9">{{ $value }}</dd>
+            <dt class="col-12 col-md-4 col-lg-3 text-md-end">
+                {{ $label }}
+            </dt>
+            <dd class="col-12 col-md-8 col-lg-9">
+                {{ is_array($value) ? 'array' : $value }}
+            </dd>
         @endforeach
     </dl>
 @endif
