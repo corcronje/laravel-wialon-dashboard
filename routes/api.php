df6dc366c5a38ad13394d06b7b6db63e9f403fec
Cor Cronje <cor@phpweb.co.za>
2024-09-14 16:17:10 +0200

    [main] Add root method as a test

--- routes/api.php
+++ routes/api.php
@@ -9,10 +9,16 @@ use App\Http\Controllers\Api\UnitController as ApiUnitController;
 use App\Http\Controllers\Api\OrderController as ApiOrderController;
 use App\Http\Middleware\EnsurePumpGuidIsPresentAndValid;
 
+Route::get('/', function () {
+    return response()->json([
+        'message' => 'Welcome to the Fuel API'
+    ]);
+});
+
 Route::middleware(EnsurePumpGuidIsPresentAndValid::class)->name('api.')->group(function () {
-    Route::resource('transactions', ApiTransactionController::class)->only(['create']);
+    Route::resource('transactions', ApiTransactionController::class)->only(['store']);
     Route::resource('drivers', ApiDriverController::class)->only(['index']);
-    Route::resource('pumps', ApiPumpController::class)->only(['index']);
+    Route::get('pumps/{guid}', [ApiPumpController::class, 'index'])->name('pumps.index');
     Route::resource('units', ApiUnitController::class)->only(['index']);
     Route::resource('orders', ApiOrderController::class)->only(['index']);
 });
