5ee468db47782654d1b581be6ae9805bf8ee1ee7
Cor Cronje <cor@phpweb.co.za>
2024-09-15 08:33:35 +0200

    [main] Refactor to traits

--- app/Models/FuelDrop.php
+++ app/Models/FuelDrop.php
@@ -2,27 +2,25 @@
 
 namespace App\Models;
 
+use App\Traits\BelongsToTank;
+use App\Traits\BelongsToUser;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class FuelDrop extends Model
 {
-    use HasFactory, SoftDeletes;
+    use HasFactory, SoftDeletes, BelongsToTank, BelongsToUser;
 
     protected $fillable = [
         'user_id',
         'tank_id',
-        'volume_in_litres',
+        'reason',
+        'volume_in_millilitres',
     ];
 
-    public function user()
+    public function getVolumeInLitresAttribute(): float
     {
-        return $this->belongsTo(User::class);
-    }
-
-    public function tank()
-    {
-        return $this->belongsTo(Tank::class);
+        return $this->volume_in_millilitres / 1000;
     }
 }
