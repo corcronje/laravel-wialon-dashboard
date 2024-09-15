b71a16fbec5aed0dfacc56bebdc5cb59692ef21d
Cor Cronje <cor@phpweb.co.za>
2024-09-15 08:53:11 +0200

    [main] Refactor and use millilitres

--- app/Models/FuelDip.php
+++ app/Models/FuelDip.php
@@ -2,27 +2,24 @@
 
 namespace App\Models;
 
+use App\Traits\BelongsToTank;
+use App\Traits\BelongsToUser;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class FuelDip extends Model
 {
-    use HasFactory, SoftDeletes;
+    use HasFactory, SoftDeletes, BelongsToTank, BelongsToUser;
 
     protected $fillable = [
         'user_id',
         'tank_id',
-        'volume_in_litres',
+        'volume_in_millilitres',
     ];
 
-    public function user()
+    public function getVolumeInLitresAttribute(): float
     {
-        return $this->belongsTo(User::class);
-    }
-
-    public function tank()
-    {
-        return $this->belongsTo(Tank::class);
+        return $this->volume_in_millilitres / 1000;
     }
 }
