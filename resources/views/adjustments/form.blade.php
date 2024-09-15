1cea341ca817267ba086c51bc18d75a09080af36
Cor Cronje <cor@phpweb.co.za>
2024-09-15 08:02:04 +0200

    [main] Add reason field

--- resources/views/adjustments/form.blade.php
+++ resources/views/adjustments/form.blade.php
@@ -1,2 +1,3 @@
 <x-select-input name="tank_id" title="Tank" :value="old('tank_id', $adjustment?->tank_id ?? '')" placeholder="Select..." :options="$tanks" />
 <x-text-input name="volume_in_litres" title="Litres" :value="old('volume_in_litres', $adjustment?->volume_in_litres ?? '')" placeholder="Litres" />
+<x-textarea-input name="reason" title="Reason" :value="old('reason', $adjustment?->reason ?? '')" placeholder="Reason" />
