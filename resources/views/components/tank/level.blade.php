ebd2afb44798c2dedab03b6c8e286fcae5b28f90
Cor Cronje <cor@phpweb.co.za>
2024-09-24 13:58:48 +0200

    [main] Fromat the persentage to two decimals

--- resources/views/components/tank/level.blade.php
+++ resources/views/components/tank/level.blade.php
@@ -2,6 +2,6 @@
 {{-- <canvas class="tank-level-chart" data-id="{{ $tank->id }}" data-percentage="{{ $tank->currentLevelPercentage }}" style="height: 200px"></canvas> --}}
 <div class="d-flex justify-content-center align-items-center">
     <h2 class="m-5">
-        {{ $tank->currentLevelPercentage }} %
+        {{ number_format($tank->currentLevelPercentage, 2) }} %
     </h2>
 </div>
