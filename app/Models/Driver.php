323a2076bfab828a4612a1b62854029d3a69dcfc
Cor Cronje <cor@phpweb.co.za>
2024-10-05 16:01:37 +0200

    [main] Update fillable prop name

--- app/Models/Driver.php
+++ app/Models/Driver.php
@@ -13,7 +13,7 @@ class Driver extends Model
 {
     use HasFactory, SoftDeletes, HasManyOrders, HasManyTrips;
 
-    protected $fillable = ['tab_id', 'employee_number', 'name', 'lastname', 'said_number'];
+    protected $fillable = ['tag_id', 'employee_number', 'name', 'lastname', 'said_number'];
 
     public function orders()
     {
